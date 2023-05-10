da2a4ca4e27764118888a84d66dde756a2626ca8
XanderVanRompaye1 <80623942+XanderVanRompaye1@users.noreply.github.com>
2023-05-10 09:47:11 +0200

    Update buildscript.sh

--- .github/scripts/buildscript.sh
+++ .github/scripts/buildscript.sh
@@ -10,19 +10,24 @@ if [ ! -d "$JAVA_HOME" ]; then
   exit 1
 fi
 
-# Build Kafka using Maven
+# Change to the Kafka source directory
 cd kafka_source_dir
-gradle
+
+# Bootstrap and download the Gradle Wrapper
+./gradlew
+
+# Verify that the Gradle Wrapper files are present
+if [ ! -f "gradlew" ] || [ ! -f "gradlew.bat" ]; then
+  echo "Error: Gradle Wrapper files not found. Make sure you have the necessary Gradle Wrapper files in your repository."
+  exit 1
+fi
+
+# Set executable permissions for the Gradle Wrapper
+chmod +x gradlew
+
+# Build the Kafka JAR
 ./gradlew jar
-./gradlew srcJar
-./gradlew aggregatedJavadoc
-./gradlew javadoc
-./gradlew javadocJar # builds a javadoc jar for each module
-./gradlew scaladoc
-./gradlew scaladocJar # builds a scaladoc jar for each module
-./gradlew docsJar # builds both (if applicable) javadoc and scaladoc jars for each module
-./gradlew installAll
-./gradlew testJar
+
 # Replace the above line with the appropriate build command for the Kafka project.
 
-# After the build completes, the Kafka distribution artifacts will be available in the 'target' directory or as specified by the build configuration.
+# After the build completes, the Kafka JAR file will be available in the 'core/build/libs' directory or as specified by the build configuration.
