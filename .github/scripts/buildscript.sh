a9e1267fec703fdde4cca7515f46bad11dae1d17
XanderVanRompaye1 <80623942+XanderVanRompaye1@users.noreply.github.com>
2023-05-10 09:49:36 +0200

    Update buildscript.sh

--- .github/scripts/buildscript.sh
+++ .github/scripts/buildscript.sh
@@ -10,24 +10,10 @@ if [ ! -d "$JAVA_HOME" ]; then
   exit 1
 fi
 
-# Change to the Kafka source directory
-cd kafka_source_dir
-
-# Bootstrap and download the Gradle Wrapper
-./gradlew
-
-# Verify that the Gradle Wrapper files are present
-if [ ! -f "gradlew" ] || [ ! -f "gradlew.bat" ]; then
-  echo "Error: Gradle Wrapper files not found. Make sure you have the necessary Gradle Wrapper files in your repository."
-  exit 1
-fi
-
-# Set executable permissions for the Gradle Wrapper
-chmod +x gradlew
-
 # Build the Kafka JAR
 ./gradlew jar
 
 # Replace the above line with the appropriate build command for the Kafka project.
+# Make sure to run the build command from the root directory of the Kafka project.
 
 # After the build completes, the Kafka JAR file will be available in the 'core/build/libs' directory or as specified by the build configuration.
